06db1ca416477f317848156eb36cd23e1fbda871
Cao Bảo Nguyên <nguyenhnal@gmail.com>
2023-12-05 13:25:19 +0700

    Add graphic, player, ground

---
+++ OOP/Assets/Script/Player.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Player : Creature
+{
+    protected override void LateStart()
+    {
+        base.LateStart();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move
+        tmpV2.x = Input.GetAxisRaw("Horizontal") * speed;
+        tmpV2.y = rb.velocity.y;
+        rb.velocity = tmpV2;
+
+        //jump
+        if(Input.GetKeyDown(KeyCode.W) && (grouding || canDoubleJump))
+        {
+            if (canDoubleJump && !grouding)
+            {
+                canDoubleJump = false;
+            }
+            tmpV2.x = rb.velocity.x;
+            tmpV2.y = canDoubleJump ? jumpForce : jumpForce / 1.2f;
+            rb.velocity = tmpV2;
+        }
+    }
+}
