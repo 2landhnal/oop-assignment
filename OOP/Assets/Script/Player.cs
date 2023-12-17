a92eb26972f55d12b4bee0d7b67389b3d093ddbe
Cao Bảo Nguyên <nguyenhnal@gmail.com>
2023-12-17 17:01:55 +0700

    xóa wall slide

--- OOP/Assets/Script/Player.cs
+++ OOP/Assets/Script/Player.cs
@@ -30,78 +30,38 @@ public class Player : Creature
     {
         if (CanControl())
         {
-            //wallslide
-            if (TouchingWall() && !grouding)
+            //move
+            tmpV2.x = horizontal * speed;
+            tmpV2.y = rb.velocity.y;
+            rb.velocity = tmpV2;
+
+            //jump
+            if (grouding || canDoubleJump)
             {
-                if (horizontal != 0)
+                if (Input.GetKeyDown(KeyCode.W))
                 {
-                    tmpV2.x = horizontal * speed;
-                    tmpV2.y = -wallSlideSpeed;
+                    if (canDoubleJump && !grouding)
+                    {
+                        canDoubleJump = false;
+                    }
+                    tmpV2.x = rb.velocity.x;
+                    tmpV2.y = canDoubleJump ? jumpForce : jumpForce / 1.2f;
                     rb.velocity = tmpV2;
-                    animator.SetBool("wallSlide", true);
                 }
-                else
+                if (Input.GetKeyUp(KeyCode.W) && rb.velocity.y > 0f)
                 {
-                    if (Input.GetKeyDown(KeyCode.W) && wallJumpCounter <= 0)
-                    {
-                        tmpV2.x = jumpForce * horizontal;
-                        tmpV2.y = canDoubleJump ? jumpForce : jumpForce / 1.2f;
-                        rb.velocity = tmpV2;
-
-                        wallJumpCounter = wallJumpCooldown;
-                    }
-                    animator.SetBool("wallSlide", false);
+                    tmpV2.x = rb.velocity.x;
+                    tmpV2.y = rb.velocity.y * .5f;
+                    rb.velocity = tmpV2;
                 }
             }
-            else
+
+            //attack
+            if (Input.GetKeyDown(KeyCode.J))
             {
-                animator.SetBool("wallSlide", false);
-            }
-
-            if (!animator.GetBool("wallSlide"))
-            {
-                //move
-                tmpV2.x = horizontal * speed;
-                tmpV2.y = rb.velocity.y;
-                rb.velocity = tmpV2;
-
-                //jump
-                if (grouding || canDoubleJump)
-                {
-                    if (Input.GetKeyDown(KeyCode.W))
-                    {
-                        if (canDoubleJump && !grouding)
-                        {
-                            canDoubleJump = false;
-                        }
-                        tmpV2.x = rb.velocity.x;
-                        tmpV2.y = canDoubleJump ? jumpForce : jumpForce / 1.2f;
-                        rb.velocity = tmpV2;
-                    }
-                    if (Input.GetKeyUp(KeyCode.W) && rb.velocity.y > 0f)
-                    {
-                        tmpV2.x = rb.velocity.x;
-                        tmpV2.y = rb.velocity.y * .5f;
-                        rb.velocity = tmpV2;
-                    }
-                }
-
-                //attack
-                if (Input.GetKeyDown(KeyCode.J))
-                {
-                    animator.SetBool("attack", true);
-                    rb.velocity = Vector2Extension.CreateVector2(rb.velocity, xToSet: 0);
-                }
+                animator.SetBool("attack", true);
+                rb.velocity = Vector2Extension.CreateVector2(rb.velocity, xToSet: 0);
             }
         }
-    }
-
-    public bool TouchingWall()
-    {
-        if (Physics2D.OverlapCircle(sidePoint.position, .2f, EnviromentProps.Instance.slideableLayers) && wallJumpCounter <= 0)
-        {
-            canDoubleJump = true;
-        }
-        return Physics2D.OverlapCircle(sidePoint.position, .2f, EnviromentProps.Instance.slideableLayers);
     }
 }
